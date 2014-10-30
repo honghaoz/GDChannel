22ce6ad7da721ad962aa581cddb6791c2d41741a
Honghao Zhang <zhh358@gmail.com>
2014-10-29 22:06:19 -0400

    Added pods

--- Demo/Demo/AppDelegate.m
+++ Demo/Demo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "GDChannel.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    id<GDCBus> bus = [[GDCSimpleBus alloc] init];
+    
+    
+    
     return YES;
 }
 
